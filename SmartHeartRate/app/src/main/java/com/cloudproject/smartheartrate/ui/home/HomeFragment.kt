f6db8a7838a6dd03896d621a1d17e5338c60aca2
KNTXTremE <kan.horwang@gmail.com>
2020-04-06 17:48:23 +0700

    Add maps into tracking fragment

--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/home/HomeFragment.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/home/HomeFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cloudproject.smartheartrate.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -22,10 +25,26 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+//        val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+//            textView.text = it
         })
+        val mapView: MapView = root.findViewById<View>(R.id.map) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+
         return root
     }
+
+    override fun onMapReady(googleMap: GoogleMap) { // Add a marker in Sydney, Australia,
+// and move the map's camera to the same location.
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions().position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
 }
