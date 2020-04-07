5a6b2e9c18194d5ab8131287f38a55088fc4e898
DDEATHQQb <teeranat.yin@gmail.com>
2020-04-07 21:38:35 +0700

    1

--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MyAdapter.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MyAdapter.kt
@@ -5,24 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.cloudproject.smartheartrate.R
 import kotlin.properties.Delegates
 
 
 class RecyclerAdapter(private val eldername_list: ArrayList<String>, private val elderrate_list: ArrayList<String>) : RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>()  {
-    class MyViewHolder(textview : View):RecyclerView.ViewHolder(textview) , View.OnClickListener{
+    class MyViewHolder(private var textview : TextView):RecyclerView.ViewHolder(textview) , View.OnClickListener{
 
-        private lateinit var eldername:String
-        private lateinit var heartrate:String
-
-        init {
-            eldername = textview.findViewById<View>(R.id.monitor_name).toString()
-            heartrate = textview.findViewById<View>(R.id.monitor_rate).toString()
-        }
-        public fun bindElder(eldernamed:String,elderrated:String){
-            this.eldername = eldernamed
-            this.heartrate = elderrated
+        fun bindElder(eldernamed:String,elderrated:String){
+            textview.findViewById<TextView>(R.id.monitor_name).text = eldernamed
+            textview.findViewById<TextView>(R.id.monitor_rate).text = elderrated
 
         }
         override fun onClick(v: View?) {
@@ -32,7 +26,7 @@ class RecyclerAdapter(private val eldername_list: ArrayList<String>, private val
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val textview = LayoutInflater.from(parent.context).inflate(R.layout.fragment_monitor,parent,false)
+        val textview = LayoutInflater.from(parent.context).inflate(R.layout.fragment_monitor,parent,false) as TextView
         return MyViewHolder(textview)
     }
 
