0c0b3b57fb8c3f9678e98eb21ce1ef2cb5a2d1e3
DDEATHQQb <->
2020-04-07 12:46:12 +0700

    save

--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorFragment.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorFragment.kt
@@ -1,6 +1,8 @@
 package com.cloudproject.smartheartrate.ui.monitor
 
+import android.media.Image
 import android.os.Bundle
+import android.os.Debug
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,23 +11,21 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cloudproject.smartheartrate.R
+import android.util.Log
 
 class MonitorFragment : Fragment() {
 
     private lateinit var monitorViewModel: MonitorViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        monitorViewModel =
-            ViewModelProviders.of(this).get(MonitorViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        monitorViewModel = ViewModelProviders.of(this).get(MonitorViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_monitor, container, false)
-        val textView: TextView = root.findViewById(R.id.text_monitor)
-        monitorViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+//        val textView: TextView = root.findViewById(R.id.text_monitor)
+
+//        monitorViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorViewModel.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorViewModel.kt
@@ -1,13 +1,19 @@
 package com.cloudproject.smartheartrate.ui.monitor
 
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MonitorViewModel : ViewModel() {
 
+    private lateinit var view:View
     private val _text = MutableLiveData<String>().apply {
         value = "This is monitor Fragment"
     }
     val text: LiveData<String> = _text
+
 }
