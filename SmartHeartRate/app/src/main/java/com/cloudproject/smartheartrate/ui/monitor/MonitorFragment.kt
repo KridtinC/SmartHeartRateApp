fbec373ef05ca6750b00a2960a39e3db4f5335f1
DDEATHQQb <->
2020-04-07 18:30:53 +0700

    big change

--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorFragment.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorFragment.kt
@@ -12,20 +12,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cloudproject.smartheartrate.R
 import android.util.Log
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
 
 class MonitorFragment : Fragment() {
-
     private lateinit var monitorViewModel: MonitorViewModel
+    private lateinit var recyclerView :RecyclerView
+    private lateinit var viewadapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager : RecyclerView.LayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         monitorViewModel = ViewModelProviders.of(this).get(MonitorViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_monitor, container, false)
-
 //        val textView: TextView = root.findViewById(R.id.text_monitor)
 
-//        monitorViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
         return root
     }
+
+
 }
