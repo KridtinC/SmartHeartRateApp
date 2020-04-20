930d66a1708bf692a401a3cd6c754e9907f0a361
DDEATHQQb <teeranat.yin@gmail.com>
2020-04-21 00:01:54 +0700

    cannot

--- SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorSendMessage.kt
+++ SmartHeartRate/app/src/main/java/com/cloudproject/smartheartrate/ui/monitor/MonitorSendMessage.kt
@@ -3,14 +3,16 @@ package com.cloudproject.smartheartrate.ui.monitor
 import android.provider.Settings.Secure.getString
 import android.util.Log
 import com.cloudproject.smartheartrate.R
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MonitorSendMessage {
+class MonitorSendMessage : FirebaseMessagingService() {
 //    private lateinit var message: FirebaseInstanceId
 
     fun getToken()  {
-        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener{task->
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener(OnCompleteListener {task->
             if(!task.isSuccessful){
                 Log.d("Message","GetInstanceFailed",task.exception)
 
@@ -18,10 +20,19 @@ class MonitorSendMessage {
                 val token = task.result?.token
                 Log.d("Message",token)
             }
-        }
+        })
 
     }
 
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        message.data.isNotEmpty().let{
+            Log.d("Message","Message data payload"+message.data)
+        }
+        message.notification?.let{
+            Log.d("Message","Message Notification Body: ${it.body}")
+        }
+    }
 
 
 
